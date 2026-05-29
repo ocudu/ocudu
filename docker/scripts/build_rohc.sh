071021b42d4205daf5f4656c3d2ba93167c7faa5
Alfredo Sáez <alfredo.saez@srs.io>
2026-05-29 07:28:35 +0000

    docker: independent stages for uhd/dpdk and cache layer optimization
    Signed-off-by: Alfredo Sáez <alfredo.saez@srs.io>

--- docker/scripts/build_rohc.sh
+++ docker/scripts/build_rohc.sh
@@ -8,26 +8,21 @@ set -e # stop executing after error
 
 main() {
     # Check number of args
-    if [ $# -lt 0 ] || [ $# -gt 2 ]; then
+    if [ $# -lt 1 ] || [ $# -gt 3 ]; then
         echo >&2 "Illegal number of parameters"
-        echo >&2 "Run like this: \"./build_rohc.sh [<arch> [<ncores>]]\" where arch is any gcc/clang compatible -march and ncores could be any number or empty for all"
+        echo >&2 "Run like this: \"./build_rohc.sh <rohc_version> [<arch> [<ncores>]]\" where arch is any gcc/clang compatible -march and ncores could be any number or empty for all"
         exit 1
     fi
 
-    local arch="${1:-native}"
-    local ncores="${2:-$(nproc)}"
+    local rohc_version=$1
+    local arch="${2:-native}"
+    local ncores="${3:-$(nproc)}"
     local rohc_name="rohc"
-    local rohc_version="2.3.1"
+    local rohc_series="${rohc_version%.*}.x"
     local rohc_archive="${rohc_name}-${rohc_version}.tar.xz"
-    local rohc_archive_sha256sum="e5c3808518239a6a4673c0c595356d5054b208f32e39015a487a0490d03f9bec"
-    local SHA256SUM_CHECKLINE="${rohc_archive_sha256sum}  ${rohc_archive}"
 
     cd /tmp
-    curl "https://rohc-lib.org/download/rohc-2.3.x/${rohc_version}/${rohc_archive}" -o "${rohc_archive}"
-    if ! echo "$SHA256SUM_CHECKLINE" | sha256sum -c -; then
-        echo "Checksum verification failed for ${rohc_archive}"
-        exit 1
-    fi
+    curl "https://rohc-lib.org/download/rohc-${rohc_series}/${rohc_version}/${rohc_archive}" -o "${rohc_archive}"
     tar -xf "${rohc_archive}"
 
     pushd "${rohc_name}-${rohc_version}"
