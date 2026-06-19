79f2717f89ef54865827906b76c1564384164fb4
Francisco Paisana <francisco.paisana@srs.io>
2026-06-19 18:26:43 +0200

    sched: maintain UE removal order in fallback_scheduler
    Signed-off-by: Francisco Paisana <francisco.paisana@srs.io>

--- lib/scheduler/ue_scheduling/ue_fallback_scheduler.cpp
+++ lib/scheduler/ue_scheduling/ue_fallback_scheduler.cpp
@@ -1572,5 +1572,6 @@ void ue_fallback_scheduler::rem_fallback_ue(du_ue_index_t ue_index)
                      pending_dl_ues_new_tx.end(),
                      [ue_index = ue_index](const fallback_ue& ue) { return ue.ue_index == ue_index; }),
       pending_dl_ues_new_tx.end());
-  pending_ul_ues.erase(std::remove(pending_ul_ues.begin(), pending_ul_ues.end(), ue_index), pending_ul_ues.end());
+  // Note: We do not use the erase+remove pattern because we want to maintain the order of pending_ul_ues.
+  pending_ul_ues.erase(std::find(pending_ul_ues.begin(), pending_ul_ues.end(), ue_index));
 }
