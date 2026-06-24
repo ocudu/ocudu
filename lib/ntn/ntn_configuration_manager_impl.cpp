e203e045e7d07965ffbc6f8281052a5b0b0aa740
Piotr Gawlowicz <piotr.gawlowicz@srs.io>
2026-06-24 18:29:19 +0200

    ntn: move SIB19 log formatter to dedicated header
    Signed-off-by: Piotr Gawlowicz <piotr.gawlowicz@srs.io>

--- lib/ntn/ntn_configuration_manager_impl.cpp
+++ lib/ntn/ntn_configuration_manager_impl.cpp
@@ -3,86 +3,15 @@
 // Portions of this file may implement 3GPP specifications, which may be subject to additional licensing requirements.
 
 #include "ntn_configuration_manager_impl.h"
+#include "ntn_log_helpers.h"
 #include "ntn_sib19_helpers.h"
 #include "ocudu/ntn/ntn_doppler_compensation_handler.h"
 #include "ocudu/ntn/ntn_sib19_update_handler.h"
 #include "ocudu/ran/sib/system_info_config.h"
-#include "ocudu/support/format/delimited_formatter.h"
 #include "fmt/chrono.h"
 
 using namespace ocudu;
 using namespace ocudu_ntn;
-
-namespace {
-
-/// Structure containing assistance information for formatting.
-struct assistance_info_wrapper {
-  slot_point                                              si_window_start;
-  slot_point                                              si_window_end;
-  slot_point                                              epoch_slot;
-  std::chrono::system_clock::time_point                   epoch_time;
-  std::optional<ta_info_t>                                ta_info;
-  std::variant<ecef_coordinates_t, orbital_coordinates_t> ephemeris_info;
-};
-
-} // namespace
-
-namespace fmt {
-
-/// \brief Custom formatter for \c assistance_info_wrapper.
-template <>
-struct formatter<assistance_info_wrapper> {
-  ocudu::delimited_formatter helper;
-
-  /// Default constructor.
-  formatter() = default;
-
-  template <typename ParseContext>
-  auto parse(ParseContext& ctx)
-  {
-    return helper.parse(ctx);
-  }
-
-  template <typename FormatContext>
-  auto format(const assistance_info_wrapper& info, FormatContext& ctx) const
-  {
-    // Format SIB19 update information.
-    helper.format_always(ctx, "si_window={}-{}", info.si_window_start, info.si_window_end);
-    helper.format_always(ctx, "epoch_slot={}", info.epoch_slot);
-    helper.format_always(ctx, "epoch_time={:%T}", info.epoch_time);
-
-    // Format TA-info.
-    if (info.ta_info.has_value()) {
-      helper.format_if_verbose(ctx, "ta_common={:.3f}us", info.ta_info->ta_common);
-      helper.format_if_verbose(ctx, "ta_common_drift={:.3f}us/s", info.ta_info->ta_common_drift);
-      helper.format_if_verbose(ctx, "ta_common_drift_variant={:.3f}us/s2", info.ta_info->ta_common_drift_variant);
-      helper.format_if_verbose(ctx, "ta_common_offset={:.3f}us", info.ta_info->ta_common_offset);
-    }
-
-    // Format ephemeris information.
-    if (std::holds_alternative<ecef_coordinates_t>(info.ephemeris_info)) {
-      const auto& ecef = std::get<ecef_coordinates_t>(info.ephemeris_info);
-      helper.format_if_verbose(ctx, "ephemeris_type=ecef");
-      helper.format_if_verbose(
-          ctx, "pos_x={:.3f}m pos_y={:.3f}m pos_z={:.3f}m", ecef.position_x, ecef.position_y, ecef.position_z);
-      helper.format_if_verbose(
-          ctx, "vel_x={:.3f}m/s vel_y={:.3f}m/s vel_z={:.3f}m/s", ecef.velocity_vx, ecef.velocity_vy, ecef.velocity_vz);
-    } else {
-      const auto& orbital = std::get<orbital_coordinates_t>(info.ephemeris_info);
-      helper.format_if_verbose(ctx, "ephemeris_type=orbital");
-      helper.format_if_verbose(ctx, "semi_major_axis={:.3f}m", orbital.semi_major_axis);
-      helper.format_if_verbose(ctx, "eccentricity={:.6f}", orbital.eccentricity);
-      helper.format_if_verbose(ctx, "periapsis={:.3f}rad", orbital.periapsis);
-      helper.format_if_verbose(ctx, "longitude={:.3f}rad", orbital.longitude);
-      helper.format_if_verbose(ctx, "mean_anomaly={:.3f}rad", orbital.mean_anomaly);
-      helper.format_if_verbose(ctx, "inclination={:.3f}rad", orbital.inclination);
-    }
-
-    return ctx.out();
-  }
-};
-
-} // namespace fmt
 
 /// \brief Compute current Doppler shift in Hz based on TA drift.
 /// \param ta_common_drift_us_per_s Timing advance drift [µs/s]
@@ -158,13 +87,13 @@ ntn_configuration_manager_impl::ntn_configuration_manager_impl(const ntn_configu
     // Check if sat_switch field is enabled.
     ctx.sat_switch_enabled = cell_config.assistance_info.sat_switch_with_resync.has_value();
 
-    if (cell_config.assistance_info.epoch_timestamp.has_value()) {
+    if (cell_config.assistance_info.epoch_timestamp) {
       ephemeris_info_update ephemeris_info;
       ephemeris_info.epoch_time     = *cell_config.assistance_info.epoch_timestamp;
       ephemeris_info.ephemeris_info = cell_config.assistance_info.ephemeris_info;
       ctx.ntn_info_generator.enqueue_ephemeris_info(ephemeris_info);
 
-      if (cell_config.assistance_info.ntn_gateway_location.has_value()) {
+      if (cell_config.assistance_info.ntn_gateway_location) {
         ntn_gateway_location_info gw_location;
         gw_location.service_start_time             = ephemeris_info.epoch_time;
         gw_location.ntn_gateway_location.latitude  = cell_config.assistance_info.ntn_gateway_location->latitude;
@@ -176,14 +105,14 @@ ntn_configuration_manager_impl::ntn_configuration_manager_impl(const ntn_configu
 
     if (ctx.sat_switch_enabled) {
       const sat_switch_with_resync_t& sat_sw = *cell_config.assistance_info.sat_switch_with_resync;
-      if (sat_sw.epoch_timestamp.has_value() && sat_sw.ntn_cfg.ephemeris_info.has_value()) {
+      if (sat_sw.epoch_timestamp && sat_sw.ntn_cfg.ephemeris_info) {
         ephemeris_info_update sat_ephemeris_update;
         sat_ephemeris_update.epoch_time     = *sat_sw.epoch_timestamp;
         sat_ephemeris_update.ephemeris_info = *sat_sw.ntn_cfg.ephemeris_info;
         if (!ctx.sat_switch_info_generator.enqueue_ephemeris_info(sat_ephemeris_update)) {
           logger.warning("Failed to enqueue sat-switch ephemeris for cell {}", cell_config.nr_cgi.nci);
         }
-        if (sat_sw.ntn_gateway_location.has_value()) {
+        if (sat_sw.ntn_gateway_location) {
           ntn_gateway_location_info sat_gw_location;
           sat_gw_location.service_start_time             = *sat_sw.epoch_timestamp;
           sat_gw_location.ntn_gateway_location.latitude  = sat_sw.ntn_gateway_location->latitude;
@@ -208,7 +137,7 @@ ntn_configuration_manager_impl::ntn_configuration_manager_impl(const ntn_configu
       }
 
       auto cur_tp_sl = time_provider->get_last_mapping(subcarrier_spacing::kHz15);
-      if (cur_tp_sl.has_value() and cur_tp_sl->slot_tx.valid()) {
+      if (cur_tp_sl and cur_tp_sl->slot_tx.valid()) {
         logger.debug("Run periodic config update task for cell {} at slot={}, time={:%T}",
                      nr_cgi.nci,
                      cur_tp_sl->slot_tx,
@@ -268,9 +197,9 @@ bool ntn_configuration_manager_impl::handle_ntn_cell_config_update(const ntn_cel
   }
 
   // Enqueue sat-switch ephemeris and gateway immediately (time-gated by sat_switch.epoch_timestamp).
-  if (cell_req.sat_switch_with_resync.has_value()) {
+  if (cell_req.sat_switch_with_resync) {
     const sat_switch_with_resync_t& sat_sw = *cell_req.sat_switch_with_resync;
-    if (sat_sw.epoch_timestamp.has_value() && sat_sw.ntn_cfg.ephemeris_info.has_value()) {
+    if (sat_sw.epoch_timestamp && sat_sw.ntn_cfg.ephemeris_info) {
       ephemeris_info_update sat_ephemeris_update;
       sat_ephemeris_update.epoch_time     = *sat_sw.epoch_timestamp;
       sat_ephemeris_update.ephemeris_info = *sat_sw.ntn_cfg.ephemeris_info;
@@ -278,7 +207,7 @@ bool ntn_configuration_manager_impl::handle_ntn_cell_config_update(const ntn_cel
         logger.warning("Failed to enqueue sat-switch ephemeris for cell {}", cell_req.nr_cgi.nci);
         return false;
       }
-      if (sat_sw.ntn_gateway_location.has_value()) {
+      if (sat_sw.ntn_gateway_location) {
         ntn_gateway_location_info sat_gw_location;
         sat_gw_location.service_start_time             = *sat_sw.epoch_timestamp;
         sat_gw_location.ntn_gateway_location.latitude  = sat_sw.ntn_gateway_location->latitude;
@@ -293,7 +222,7 @@ bool ntn_configuration_manager_impl::handle_ntn_cell_config_update(const ntn_cel
   }
 
   // If provided, send NTN gateway location info to SIB19 NTN config generator.
-  if (cell_req.ntn_gateway_location.has_value()) {
+  if (cell_req.ntn_gateway_location) {
     ntn_gateway_location_info gw_location;
     gw_location.service_start_time             = cell_req.epoch_time;
     gw_location.ntn_gateway_location.latitude  = cell_req.ntn_gateway_location->latitude;
@@ -315,37 +244,37 @@ void ntn_configuration_manager_impl::apply_deferred_cell_config_update(per_cell_
                                                                        const ntn_cell_config_update_info& update)
 {
   ctx.cell_cfg.assistance_info.ntn_ul_sync_validity_dur = update.ntn_ul_sync_validity_duration;
-  if (update.ta_info.has_value()) {
+  if (update.ta_info) {
     ctx.cell_cfg.assistance_info.ta_info = update.ta_info;
   }
-  if (update.feeder_link_info.has_value()) {
+  if (update.feeder_link_info) {
     ctx.cell_cfg.assistance_info.feeder_link_info = update.feeder_link_info;
   }
-  if (update.reference_location.has_value()) {
+  if (update.reference_location) {
     ctx.cell_cfg.assistance_info.reference_location = update.reference_location;
   }
-  if (update.distance_threshold.has_value()) {
+  if (update.distance_threshold) {
     ctx.cell_cfg.assistance_info.distance_threshold = update.distance_threshold;
   }
-  if (update.t_service.has_value()) {
+  if (update.t_service) {
     ctx.cell_cfg.assistance_info.t_service = update.t_service;
   }
-  if (update.polarization.has_value()) {
+  if (update.polarization) {
     ctx.cell_cfg.assistance_info.polarization = update.polarization;
   }
-  if (update.ta_report.has_value()) {
+  if (update.ta_report) {
     ctx.cell_cfg.assistance_info.ta_report = update.ta_report;
   }
-  if (update.ncells.has_value()) {
+  if (update.ncells) {
     ctx.cell_cfg.assistance_info.ncells.clear();
     for (const auto& ncell : *update.ncells) {
       ctx.cell_cfg.assistance_info.ncells.push_back(ncell);
     }
   }
-  if (update.moving_ref_location.has_value()) {
+  if (update.moving_ref_location) {
     ctx.cell_cfg.assistance_info.moving_reference_location = update.moving_ref_location;
   }
-  if (update.sat_switch_with_resync.has_value()) {
+  if (update.sat_switch_with_resync) {
     ctx.cell_cfg.assistance_info.sat_switch_with_resync = update.sat_switch_with_resync;
     ctx.sat_switch_enabled                              = true;
   } else {
@@ -358,7 +287,7 @@ bool ntn_configuration_manager_impl::send_cfo_compensation_request(const per_cel
                                                                    time_point              doppler_update_time,
                                                                    const ta_info_t&        ta_info)
 {
-  if (not ctx.cell_cfg.assistance_info.feeder_link_info.has_value()) {
+  if (not ctx.cell_cfg.assistance_info.feeder_link_info) {
     return false;
   }
 
@@ -381,7 +310,7 @@ bool ntn_configuration_manager_impl::send_cfo_compensation_request(const per_cel
                                                                ctx.cell_cfg.assistance_info.feeder_link_info->ul_freq);
 
   // Check if sector_id is configured, warn if missing.
-  if (!ctx.cell_cfg.sector_id.has_value()) {
+  if (!ctx.cell_cfg.sector_id) {
     logger.warning("Cell {} has no sector_id configured, using default value 0 for Doppler compensation",
                    ctx.cell_cfg.nr_cgi.nci);
   }
@@ -497,14 +426,14 @@ void ntn_configuration_manager_impl::periodic_ntn_config_update_task(const nr_ce
     sib19.ntn_cfg->ta_info                     = reply.ta_info;
     sib19.ntn_cfg->ntn_ul_sync_validity_dur    = reply.ntn_ul_sync_validity_dur;
 
-    if (ctx.cell_cfg.assistance_info.ta_info.has_value() and ctx.cell_cfg.assistance_info.ta_info->ta_common_offset) {
-      if (!sib19.ntn_cfg->ta_info.has_value()) {
+    if (ctx.cell_cfg.assistance_info.ta_info and ctx.cell_cfg.assistance_info.ta_info->ta_common_offset) {
+      if (!sib19.ntn_cfg->ta_info) {
         sib19.ntn_cfg->ta_info.emplace();
       }
       sib19.ntn_cfg->ta_info->ta_common_offset = ctx.cell_cfg.assistance_info.ta_info->ta_common_offset;
     }
 
-    if (ctx.sat_switch_enabled && sib19.sat_switch_with_resync.has_value() &&
+    if (ctx.sat_switch_enabled && sib19.sat_switch_with_resync &&
         sib19.sat_switch_with_resync->epoch_timestamp.has_value() &&
         sib19.sat_switch_with_resync->ntn_cfg.ephemeris_info.has_value()) {
       auto&                     sat_sw = *sib19.sat_switch_with_resync;
@@ -533,7 +462,7 @@ void ntn_configuration_manager_impl::periodic_ntn_config_update_task(const nr_ce
     // If neighbors are present in SIB19, populate the first neighbor NTN config
     // with the serving-cell NTN config parameters. Specifically, neighboring cell use the same satellite.
     if (!sib19.ncells.empty()) {
-      if (!sib19.ncells[0].ntn_cfg.has_value()) {
+      if (!sib19.ncells[0].ntn_cfg) {
         sib19.ncells[0].ntn_cfg.emplace();
       }
       sib19.ncells[0].ntn_cfg->ephemeris_info = sib19.ntn_cfg->ephemeris_info;
@@ -550,7 +479,7 @@ void ntn_configuration_manager_impl::periodic_ntn_config_update_task(const nr_ce
   }
 
   // Send CFO compensation request to PHY.
-  if (reply.ta_info.has_value()) {
+  if (reply.ta_info) {
     send_cfo_compensation_request(ctx, epoch_time, *reply.ta_info);
   }
 }
---
+++ lib/ntn/ntn_log_helpers.h
@@ -0,0 +1,85 @@
+// SPDX-FileCopyrightText: Copyright (C) 2021-2026 Software Radio Systems Limited
+// SPDX-License-Identifier: BSD-3-Clause-Open-MPI
+// Portions of this file may implement 3GPP specifications, which may be subject to additional licensing requirements.
+
+#pragma once
+
+#include "ocudu/ran/ntn.h"
+#include "ocudu/ran/slot_point.h"
+#include "ocudu/support/format/delimited_formatter.h"
+#include <chrono>
+#include <variant>
+
+namespace ocudu {
+namespace ocudu_ntn {
+
+/// Structure containing assistance information for formatting.
+struct assistance_info_wrapper {
+  slot_point                                              si_window_start;
+  slot_point                                              si_window_end;
+  slot_point                                              epoch_slot;
+  std::chrono::system_clock::time_point                   epoch_time;
+  std::optional<ta_info_t>                                ta_info;
+  std::variant<ecef_coordinates_t, orbital_coordinates_t> ephemeris_info;
+};
+
+} // namespace ocudu_ntn
+} // namespace ocudu
+
+namespace fmt {
+
+/// \brief Custom formatter for \c assistance_info_wrapper.
+template <>
+struct formatter<ocudu::ocudu_ntn::assistance_info_wrapper> {
+  ocudu::delimited_formatter helper;
+
+  /// Default constructor.
+  formatter() = default;
+
+  template <typename ParseContext>
+  auto parse(ParseContext& ctx)
+  {
+    return helper.parse(ctx);
+  }
+
+  template <typename FormatContext>
+  auto format(const ocudu::ocudu_ntn::assistance_info_wrapper& info, FormatContext& ctx) const
+  {
+    // Format SIB19 update information.
+    helper.format_always(ctx, "si_window={}-{}", info.si_window_start, info.si_window_end);
+    helper.format_always(ctx, "epoch_slot={}", info.epoch_slot);
+    helper.format_always(ctx, "epoch_time={:%T}", info.epoch_time);
+
+    // Format TA-info.
+    if (info.ta_info) {
+      helper.format_if_verbose(ctx, "ta_common={:.3f}us", info.ta_info->ta_common);
+      helper.format_if_verbose(ctx, "ta_common_drift={:.3f}us/s", info.ta_info->ta_common_drift);
+      helper.format_if_verbose(ctx, "ta_common_drift_variant={:.3f}us/s2", info.ta_info->ta_common_drift_variant);
+      helper.format_if_verbose(ctx, "ta_common_offset={:.3f}us", info.ta_info->ta_common_offset);
+    }
+
+    // Format ephemeris information.
+    if (const auto* ecef = std::get_if<ocudu::ecef_coordinates_t>(&info.ephemeris_info)) {
+      helper.format_if_verbose(ctx, "ephemeris_type=ecef");
+      helper.format_if_verbose(
+          ctx, "pos_x={:.3f}m pos_y={:.3f}m pos_z={:.3f}m", ecef->position_x, ecef->position_y, ecef->position_z);
+      helper.format_if_verbose(ctx,
+                               "vel_x={:.3f}m/s vel_y={:.3f}m/s vel_z={:.3f}m/s",
+                               ecef->velocity_vx,
+                               ecef->velocity_vy,
+                               ecef->velocity_vz);
+    } else if (const auto* orbital = std::get_if<ocudu::orbital_coordinates_t>(&info.ephemeris_info)) {
+      helper.format_if_verbose(ctx, "ephemeris_type=orbital");
+      helper.format_if_verbose(ctx, "semi_major_axis={:.3f}m", orbital->semi_major_axis);
+      helper.format_if_verbose(ctx, "eccentricity={:.6f}", orbital->eccentricity);
+      helper.format_if_verbose(ctx, "periapsis={:.3f}rad", orbital->periapsis);
+      helper.format_if_verbose(ctx, "longitude={:.3f}rad", orbital->longitude);
+      helper.format_if_verbose(ctx, "mean_anomaly={:.3f}rad", orbital->mean_anomaly);
+      helper.format_if_verbose(ctx, "inclination={:.3f}rad", orbital->inclination);
+    }
+
+    return ctx.out();
+  }
+};
+
+} // namespace fmt
